0e93cf1d1eb647df98c919749768569c692e5994
muthanna Aljbory <mothnanet@gmail.com>
2017-10-24 12:15:25 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "gueen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [] ;
+
+//check for a match
+var checkForMatch = function() {
+            // do they match?
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("You found a match!")
+        } else {
+          alert("Sorry, try again.")
+      }
+}
+
+// code for flipping the card and storing volue
+var flipCard = function (cardID) {
+
+    cardsInPlay.push(cards[cardID]);
+    console.log("User flipped " + cards[cardID]);
+
+        //find number of cards played
+        if (cardsInPlay.length === 2) {
+            checkForMatch();
+        }
+}
+
+flipCard(0);
+flipCard(2);
