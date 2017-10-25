eac6f1a1705e821f1a3c3dafda88dbc4a85a1926
muthanna Aljbory <mothnanet@gmail.com>
2017-10-25 11:55:51 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-var cards = ["queen", "queen", "king", "king"];
+// code writting an array
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png",
+  };
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png'",
+  };
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+  };
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+  };
+
+];
 var cardsInPlay = [] ;
 
 //check for a match
 var checkForMatch = function() {
-            // do they match?
+
         if (cardsInPlay[0] === cardsInPlay[1]) {
           alert("You found a match!")
         } else {
@@ -14,8 +37,10 @@ var checkForMatch = function() {
 // code for flipping the card and storing volue
 var flipCard = function (cardID) {
 
-    cardsInPlay.push(cards[cardID]);
-    console.log("User flipped " + cards[cardID]);
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId]).rank;
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 
         //find number of cards played
         if (cardsInPlay.length === 2) {
